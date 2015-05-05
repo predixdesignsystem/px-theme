aafe4c4c8542ff8804f10a858df1191b154e5641
Martin Wragg <martin.wragg@ge.com>
2015-05-05 09:47:55 -0700

    app/page level CSS workflow experiment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,9 +21,20 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    'css/px-theme.css': 'sass/px-theme.scss'
-                }
+                    'css/noprefix/px-app.css': 'sass/px-page-theme.scss'                }
             }
+        },
+
+        autoprefixer: {
+          options: {
+            browsers: ['last 2 version']
+          },
+          multiple_files: {
+            expand: true,
+            flatten: true,
+            src: 'css/noprefix/*.css',
+            dest: 'css'
+          }
         },
 
         shell: {
@@ -63,18 +74,29 @@ module.exports = function (grunt) {
         },
 
         insert: {
-          insertcss:{
+          insertcomponentcss:{
             src: "css/px-theme.css",
             dest: "px-theme.html",
+            match: "!!css goes here!!"
+          },
+          insertappcss:{
+            src: "css/px-app.css",
+            dest: "px-app.html",
             match: "!!css goes here!!"
           }
         },
 
         copy: {
-          copyhtmltemplate: {
+          copycomponenttemplate: {
             src: "_px-theme.html",
             dest: "px-theme.html"
+          },
+          copyapptemplate: {
+            src: "_px-app.html",
+            dest: "px-app.html"
           }
+
+
         }
     });
 
@@ -84,13 +106,15 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-dep-serve');
-    grunt.loadNpmTasks('grunt-insert');    
+    grunt.loadNpmTasks('grunt-insert');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-autoprefixer');
 
     // Default task.
     grunt.registerTask('default', 'Basic build', [
       'clean',
       'sass',
+      'autoprefixer',
       'copy',
       'insert'
     ]);
